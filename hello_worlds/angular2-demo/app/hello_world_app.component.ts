8b536849d49e260009cd9f78191469094700f57b
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-03-16 18:55:19 +0100

    added/changed some files

---
+++ hello_worlds/angular2-demo/app/hello_world_app.component.ts
@@ -0,0 +1,14 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Hello World !!</h2>'
+})
+
+export class MyHelloWorldClass {
+
+}
+
---
+++ hello_worlds/angular2-demo/app/hello_world_main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from "angular2/platform/browser"
+import {MyHelloWorldClass} from "./hello_world_app.component"
+
+bootstrap(MyHelloWorldClass);
+
