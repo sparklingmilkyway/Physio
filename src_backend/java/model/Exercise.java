4e55927d7b6a1061fed4f3f19d59fe2d76754285
nydej2 <jonas.nydegger@students.bfh.ch>
2017-03-19 12:34:16 +0100

    designed Exercise.java

--- src_backend/java/model/Exercise.java
+++ src_backend/java/model/Exercise.java
@@ -1,3 +1,39 @@
+package java.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise {
 
+    private String name;
+    private String type;
+    private List<String> notes;
+
+    public Exercise(String name,String type){
+        this.name = name;
+        this.type = type;
+        notes = new ArrayList<String>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void AddNote(String note){
+        notes.add(note);
+    }
+
+    public boolean RemoveNote(String note){
+        for(String n : this.notes){
+            if(n.equals(note)){
+                notes.remove(n);
+                return true;
+            }
+        }
+        return false;
+    }
 }
