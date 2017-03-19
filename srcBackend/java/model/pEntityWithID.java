aa3f90dce42bf601cc3ee6c41d15f7384eb1a671
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-03-19 20:42:57 +0100

    added Entity abstract class and interface

---
+++ srcBackend/java/model/pEntityWithID.java
@@ -0,0 +1,21 @@
+package java.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public abstract class pEntityWithID implements pEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
