054fd88dc308ea2d2048deaf9748e5e5feaa6df9
nydej2 <jonas.nydegger@students.bfh.ch>
2017-03-27 15:09:06 +0200

    ExerciseManager added RemoveNote,AddNote,getExerciseById methods

--- srcBackend/java/manager/ExerciseManager.java
+++ srcBackend/java/manager/ExerciseManager.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.concept.Exercise;
+import java.util.List;
 
 /**
  * Created by Vanessa on 19.03.17.
@@ -15,19 +16,26 @@ public class ExerciseManager {
     private EntityManager em = emf.createEntityManager(); // Retrieve an application managed entity manager
 
 
-    public void AddNote(String note){
-        //notes.add(note);
+    public void AddNote(String note, Exercise exercise){
+        Exercise ex = getExerciseById(exercise.getId());
+        List<String> notes = ex.getNotes();
+        notes.add(note);
+        ex.setNotes(notes);
+        save(ex);
     }
 
-    public boolean RemoveNote(String note){
-       /** for(String n : this.notes){
+    public boolean RemoveNote(String note, Exercise exercise){
+        Exercise ex = getExerciseById(exercise.getId());
+        List<String> notes = ex.getNotes();
+        for(String n: notes){
             if(n.equals(note)){
-                notes.remove(n);
+                notes.remove(note);
+                ex.setNotes(notes);
+                save(ex);
                 return true;
             }
-        }*/
+        }
         return false;
-
     }
 
     public void addNote(Exercise ex, String note){
@@ -37,7 +45,6 @@ public class ExerciseManager {
     public Exercise getExe(Exercise ex, String note){
 
     }
-
 
     public Exercise newExercise(String name,String type) {
         Exercise ex = new Exercise();
@@ -50,8 +57,10 @@ public class ExerciseManager {
         em.persist(ex);
     }
 
-
-
+    public Exercise getExerciseById(long id){
+        Exercise ex = em.find(Exercise.class,id);
+        return ex;
+    }
 
     public void test(){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1")
