339c3eef1b490763fb4b41d143ad2700ff2bb32d
nydej2 <jonas.nydegger@students.bfh.ch>
2017-03-28 10:59:39 +0200

    added methods removeProgrammExponentByExercise& removeProgrammExponent to ProgrammManager

--- srcBackend/java/manager/ProgrammManager.java
+++ srcBackend/java/manager/ProgrammManager.java
@@ -1,39 +1,76 @@
 package java.manager;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.concept.Exercise;
+import java.concept.Praxis;
+import java.concept.Programm;
 import java.concept.ProgrammExponent;
+import java.util.List;
 
 public class ProgrammManager {
 
-    private EntityManager entityManager = new EntityManager();
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+    private EntityManager em = emf.createEntityManager(); // Retrieve an application managed entity manager
 
-    public ProgrammExponent addExercise(Exercise exercise, int sets, int reps){
-        ProgrammExponent proExp = new  ProgrammExponent(exercise, sets, reps);
-        this.exerciseList.add(proExp);
-        return proExp;
+    public void addExercise(ProgrammExponent programmExponent, int sets, int reps, long id){
+        Programm pr = getProgrammById(id);
+        List<ProgrammExponent> exerciseList = pr.getExerciseList();
+        exerciseList.add(programmExponent);
+        pr.setExerciseList(exerciseList);
+        save(pr);
     }
 
-    public boolean removeProgrammExponentByExercise(Exercise exercise){
-        for (int i = 0; i < exerciseList.size(); i++) {
-            ProgrammExponent progExp = new ProgrammExponent();
-            if (exerciseList.get(i).getExercise()==exercise) {
-                progExp = exerciseList.get(i);
+    public boolean removeProgrammExponentByExercise(Exercise exercise, long id){
+
+        //Hie bini mr nid sicher obi di richtig verstande ha mit de Exponente
+
+        Programm pr = getProgrammById(id);
+        List<ProgrammExponent> programmExponentList = pr.getExerciseList();
+
+        for(int i = 0; i <= programmExponentList.size(); i++){
+            if(programmExponentList.get(i).equals(exercise)){
+                programmExponentList.remove(i);
+                pr.setExerciseList(programmExponentList);
+                save(pr);
+                return true;
             }
         }
+        return false;
+    }
 
-        if(this.exerciseList.contains(progExp)){
-            this.exerciseList.remove(progExp);
+    public boolean removeProgrammExponent(ProgrammExponent exercise, long id){
+        Programm pr = getProgrammById(id);
+        List<ProgrammExponent> programmExponentList = pr.getExerciseList();
+        if(programmExponentList.contains(exercise)){
+            programmExponentList.remove(exercise);
+            pr.setExerciseList(programmExponentList);
+            save(pr);
             return true;
         }
         return false;
     }
 
-    public boolean removeProgrammExponent(ProgrammExponent exercise){
-        if(this.exerciseList.contains(exercise)){
-            this.exerciseList.remove(exercise);
-            return true;
-        }
-        return false;
+    public void save(Programm programm) {
+        em.persist(programm);
+    }
+
+    private Programm getProgrammById(long id){
+        Programm pr = em.find(Programm.class,id);
+        return pr;
+    }
+
+    public void test(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+        EntityManager em = emf.createEntityManager(); // Retrieve an application managed entity manager
+
+
+        // Work with the EM
+        em.close();
+
+        emf.close(); //close at application end
+
     }
 }
 
