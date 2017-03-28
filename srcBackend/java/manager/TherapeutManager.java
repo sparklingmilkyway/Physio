4d2b063d4d34175e308c23b041a7e7de327946df
nydej2 <jonas.nydegger@students.bfh.ch>
2017-03-28 11:31:14 +0200

    Added methods addPatient, removePatient, changePasswordHash, getPasswordHash, createExercise, removeExercise, saveExercise, remove to TherapeutManager

--- srcBackend/java/manager/TherapeutManager.java
+++ srcBackend/java/manager/TherapeutManager.java
@@ -1,49 +1,102 @@
 package java.manager;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.concept.Exercise;
 import java.concept.Patient;
+import java.concept.Praxis;
+import java.concept.Therapeut;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Vanessa on 19.03.17.
  */
 public class TherapeutManager {
 
-    private EntityManager entityManager = new EntityManager();
+    //private EntityManager entityManager = new EntityManager();
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+    private EntityManager em = emf.createEntityManager(); // Retrieve an application managed entity manager
 
-    public void addPatient(Patient patient){
-        if(patientList.contains(patient)){
-            return;
-        }
+    public void addPatient(Patient patient, long id){
+        Therapeut te = getTherapeutById(id);
+        Set<Patient> patientList = te.getPatientList();
         patientList.add(patient);
+        te.setPatientList(patientList);
+        save(te);
     }
 
-    public boolean removePatient(Patient patient){
-        if(patientList.contains(patient)){
-            patientList.remove(patient);
+    public boolean removePatient(Patient patient, long id){
+        Therapeut te = getTherapeutById(id);
+        Set<Patient> patientSet = te.getPatientList();
+        if(patientSet.contains(patient)) {
+            patientSet.remove(patient);
+            te.setPatientList(patientSet);
+            save(te);
             return true;
         }
         return false;
     }
 
-    public void changePasswordHash(String passwordHash){
-        this.passwordHash = passwordHash;
+    //chönnte mr die methode nid grad la si? chame ja mit de getter&setter regle
+    public void changePasswordHash(String passwordHash, long id){
+        Therapeut te = getTherapeutById(id);
+        te.setPasswordHash(passwordHash);
+        save(te);
     }
 
-    public String getPasswordHash(){
-        return this.passwordHash;
+    // chönnte mr die Methode nid grad lasi? chame ja mit de getter&setter regle
+    public String getPasswordHash(long id){
+        Therapeut te = getTherapeutById(id);
+        return te.getPasswordHash();
     }
 
-    public boolean addExercise(String name, String type){
-        Exercise e;
-        e = new Exercise(name, type);
-        /*We'll need some persistence manager later which adds the exercise to the DB*/
-        return true;
+    //oh hie gits sicher no e besseri lösig...
+    public void createExercise(String name, String type){
+        Exercise exercise = new Exercise(name,type);
+        saveExercise(exercise);
     }
 
-    public boolean removeExercise(String name, String type){
-         /*We'll need some persistence manager later which removes the exercise to the DB*/
-        return true;
+    public void removeExercise(long id){
+            Exercise ex = getExerciseById(id);
+            remove(ex);
     }
+
+    public void save(Therapeut therapeut) {
+        em.persist(therapeut);
+    }
+
+    public void remove(Exercise exercise){
+        em.remove(exercise);
+    }
+
+    public void saveExercise(Exercise exercise){
+        em.persist(exercise);
+    }
+
+    private Therapeut getTherapeutById(long id){
+        Therapeut te = em.find(Therapeut.class,id);
+        return te;
+    }
+
+    private Exercise getExerciseById(long id){
+        Exercise ex = em.find(Exercise.class, id);
+        return ex;
+    }
+
+    public void test(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
+        EntityManager em = emf.createEntityManager(); // Retrieve an application managed entity manager
+
+
+        // Work with the EM
+        em.close();
+
+        emf.close(); //close at application end
+
+    }
+
 
 
 }
