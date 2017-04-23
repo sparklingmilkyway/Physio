4c2218e8f963c3ce0cb4746d4137e68014fee3d7
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-04-23 19:39:38 +0200

    first API steps with JAX RS // created ApiApplication, HelloWorldResource, ProgrammExerciseResource, UserResource

---
+++ backend/src/main/java/ch/ti/bfh/physio_app/api/ApiApplication.java
@@ -0,0 +1,24 @@
+package ch.ti.bfh.physio_app.api;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@ApplicationPath("/api")
+public class ApiApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return newHashSet(
+                HelloWorldResource.class,
+                UserResource.class,
+                ProgrammExerciseResource.class,
+                ApiApplication.class
+        );
+    }
+}
---
+++ backend/src/main/java/ch/ti/bfh/physio_app/api/ProgrammExerciseResource.java
@@ -0,0 +1,92 @@
+package ch.ti.bfh.physio_app.api;
+
+import ch.ti.bfh.physio_app.concept.Exercise;
+import ch.ti.bfh.physio_app.concept.Programm;
+import ch.ti.bfh.physio_app.manager.ExerciseManager;
+
+import javax.inject.Inject;
+import javax.ws.rs.*; import javax.ws.rs.core.Response;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.ok;
+import static javax.ws.rs.core.Response.status;
+
+@Path("")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+public class ProgrammExerciseResource {
+
+    @Inject
+    private ExerciseManager exerciseManager;
+
+    /** Programm **/
+    @GET
+    @Path("/programm")
+    public Response programm() {
+        // get a set of programm from service
+        return ok("Programm site").build();
+    }
+
+    @GET
+    @Path("/programm/{programmId}")
+    public Response patient(@PathParam("programmId") long programmId) {
+        //get programm from service
+        Programm programm = new Programm();
+        programm.setId(programmId);
+        return ok(programm).build();
+    }
+
+
+    @POST
+    @Path("/programm/{programmId}")
+    public Response patient(@PathParam("programmId") long programmId, Programm programm) {
+        // give patient + patientId to service
+        return ok("Success/Faild").build();
+    }
+
+    /** Exercise **/
+
+    @GET
+    @Path("/exercise")
+    public Response exercise() {
+
+        Exercise ex1 = new Exercise("Exercise 1", "Rücken");
+        System.out.println(ex1);
+        exerciseManager.save(ex1);
+        Exercise ex2 = new Exercise("Exercise 2", "Rücken");
+        System.out.println(ex2);
+        exerciseManager.save(ex2);
+        Exercise ex3 = new Exercise("Exercise 3", "Rücken");
+        System.out.println(ex3);
+        exerciseManager.save(ex3);
+
+
+        //exerciseManager.start();
+        return ok("Exercise site").build();
+    }
+
+    @GET
+    @Path("/exercise/{exerciseId}")
+    public Response exercise(@PathParam("exerciseId") long exerciseId) {
+        //get exercise from service
+        Exercise exercise = exerciseManager.getExerciseById(exerciseId);
+        return ok(exercise).build();
+    }
+
+    @POST
+    @Path("/{exerciseId}")
+    public Response exercise(@PathParam("exerciseId") long exerciseId, Exercise exercise) {
+        // give exercise + exerciseId to service
+        return ok("Success/Faild").build();
+    }
+
+    /** ProgrammExponent **/
+
+    @GET
+    @Path("/programm/{programmId}/exercises/{programmExponentId}")
+    public Response patient(@PathParam("programmId") long programmId, @PathParam("programmExponentId") long programmExponentId) {
+        //get programm exponent from service
+        return ok("get programm exponent from service").build();
+    }
+
+}
