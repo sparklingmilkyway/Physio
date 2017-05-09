fdc7c6968829dcc31f1d8f847616a6f517a4a2ac
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-05-09 16:32:35 +0200

    corrected some annotations

--- backend/src/main/java/ch/ti/bfh/physio_app/concept/Praxis.java
+++ backend/src/main/java/ch/ti/bfh/physio_app/concept/Praxis.java
@@ -3,6 +3,9 @@ package ch.ti.bfh.physio_app.concept;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.Set;
 /**
  * This class describes the attributes and function of a Praxis.
  */
+@XmlRootElement(name = "Praxis")
 @Entity
 public class Praxis extends pEntityWithID{
 
@@ -30,6 +34,7 @@ public class Praxis extends pEntityWithID{
         therapeuts = new HashSet<Therapeut>();
     }
 
+    @XmlElement(name = "name")
     public String getName() {
         return name;
     }
@@ -38,6 +43,8 @@ public class Praxis extends pEntityWithID{
         this.name = name;
     }
 
+    @XmlElementWrapper(name = "therapeuts")
+    @XmlElement(name = "therapeut")
     public Set<Therapeut> getTherapeuts() {
         return therapeuts;
     }
--- backend/src/main/java/ch/ti/bfh/physio_app/concept/pEntityWithID.java
+++ backend/src/main/java/ch/ti/bfh/physio_app/concept/pEntityWithID.java
@@ -1,6 +1,7 @@
 package ch.ti.bfh.physio_app.concept;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  * Every Component of our concept will inherit this abstract class. The pEntityWithID class is to give each object an
@@ -8,6 +9,7 @@ import javax.persistence.*;
  * */
 @MappedSuperclass
 public abstract class pEntityWithID implements pEntity {
+    @XmlElement(name = "id")
     @Id /* @Id declares the identifier property of this entity. */
     @GeneratedValue(strategy=GenerationType.IDENTITY) /* @GeneratedValue annotation is used to specify the primary key generation strategy to use. If the strategy is not specified by default AUTO will be used. */
     @Column /* @Column annotation is used to specify the details of the column to which a field or property will be mapped. If the @Column annotation is not specified by default the property name will be used as the column name. */
