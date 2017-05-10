f6c3da841a0693335d6d0aa1bf87f8018aa71a15
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-05-10 11:18:31 +0200

    added praxis.service.ts to frontend

---
+++ hello_worlds/sandbox/src/app/praxis.service.ts
@@ -0,0 +1,35 @@
+import {Injectable, OnInit} from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PraxisService implements OnInit {
+
+  ngOnInit() {
+  }
+
+  constructor (
+    private http: Http
+  ) {}
+
+  addPraxis() {
+    /*return this.http.get(`http://127.0.0.1:8080/api/praxis/add`)
+      .map((res:Response) => res.json());*/
+
+    return this.http.request(`http://127.0.0.1:8080/api/praxis/add`)
+      .map((res:Response) => res.json());
+  }
+
+  addTherapeuts() {
+
+    return this.http.request(`http://127.0.0.1:8080/api/praxis/addTherapeuts`)
+      .map((res:Response) => res.json());
+  }
+
+  getTherapeuts() {
+
+    return this.http.request(`http://127.0.0.1:8080/api/praxis/getTherapeuts`)
+      .map((res:Response) => res.json());
+  }
+
+}
