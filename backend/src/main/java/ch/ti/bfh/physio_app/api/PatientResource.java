3ff9addb45f83d46d6dd5988a74ee1ae9bf8cf25
nydej2 <jonas.nydegger@students.bfh.ch>
2017-05-16 20:27:24 +0200

    created rest method for getting users from id

--- backend/src/main/java/ch/ti/bfh/physio_app/api/PatientResource.java
+++ backend/src/main/java/ch/ti/bfh/physio_app/api/PatientResource.java
@@ -1,19 +1,19 @@
 package ch.ti.bfh.physio_app.api;
 
-        import ch.ti.bfh.physio_app.concept.Patient;
-        import ch.ti.bfh.physio_app.concept.Praxis;
-        import ch.ti.bfh.physio_app.concept.Therapeut;
-        import ch.ti.bfh.physio_app.manager.PatientManager;
-        import ch.ti.bfh.physio_app.manager.PraxisManager;
-        import ch.ti.bfh.physio_app.manager.TherapeutManager;
+import ch.ti.bfh.physio_app.concept.Patient;
+import ch.ti.bfh.physio_app.concept.Praxis;
+import ch.ti.bfh.physio_app.concept.Therapeut;
+import ch.ti.bfh.physio_app.manager.PatientManager;
+import ch.ti.bfh.physio_app.manager.PraxisManager;
+import ch.ti.bfh.physio_app.manager.TherapeutManager;
 
 
-        import javax.inject.Inject;
-        import javax.ws.rs.*;
-        import javax.ws.rs.core.Response;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 
-        import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-        import static javax.ws.rs.core.Response.ok;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.ok;
 
 @Path("/patient")
 @Consumes(APPLICATION_JSON)
@@ -30,17 +30,22 @@ public class PatientResource {
     private PatientManager patientManager;
 
     @GET
-    @Path("/add")
-    public Response addPatient() {
+    @Path("/add/sn={surname}/ls={lastname}")
+    public Response addPatient(@PathParam("surname") String surname, @PathParam("lastname") String lastname) {
         long id = 1;
         Praxis praxis = praxisManager.getPraxisById(id);
         Therapeut therapeut = therapeutManager.getTherapeutById(id);
-        Patient patient = new Patient("Jonas","Nydegger", "jonas@mail.ch", "pwhs", therapeut);
+        Patient patient = new Patient(surname,lastname, "jonas@mail.ch", "pwhs", therapeut);
         patientManager.save(patient);
         return ok(patient).build();
-
     }
 
+    @GET
+    @Path("/get/id={id}")
+    public Response getPatient(@PathParam("id") long id){
+        Patient patient = patientManager.getPatientById(id);
+        return ok(patient).build();
+    }
 
 
 }
