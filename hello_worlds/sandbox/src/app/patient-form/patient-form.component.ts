5f36e64ee69f316036ca80567981247e936d21e7
nydej2 <jonas.nydegger@students.bfh.ch>
2017-05-29 18:42:08 +0200

    forgot some stuff for popups..

--- hello_worlds/sandbox/src/app/patient-form/patient-form.component.ts
+++ hello_worlds/sandbox/src/app/patient-form/patient-form.component.ts
@@ -32,13 +32,13 @@ export class PatientFormComponent implements OnInit {
   clickButton(){
 
     this.popup1.options = {
-      header: "Details für Patient "+this.selectedPatient.lastname,
-      color: "#E2F0F5", // red, blue....
+      header: "Details für Patient "+this.selectedPatient.firstname,
+      color: "lightblue", // red, blue....
       widthProsentage: 40, // The with of the popou measured by browser width
       animationDuration: 0.5, // in seconds, 0 = no animation
       showButtons: true, // You can hide this in case you want to use custom buttons
       confirmBtnContent: "Änderungen speichern", // The text on your confirm button
-      cancleBtnContent: "Cancel", // the text on your cancel button
+      cancleBtnContent: "Abbrechen", // the text on your cancel button
       confirmBtnClass: "btn btn-default", // your class for styling the confirm button
       cancleBtnClass: "btn btn-default", // you class for styling the cancel button
       animation: "fadeInDown" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
@@ -73,4 +73,10 @@ export class PatientFormComponent implements OnInit {
   onSelect(patient: Patient){
     this.selectedPatient = patient;
   }
+
+  changePatient(){
+    this.patientService.changePatient(this.selectedPatient.id,this.selectedPatient.therapeut,this.selectedPatient.surname,this.selectedPatient.firstname,this.selectedPatient.email).subscribe(data => this.patient = data);
+    location.reload();
+  }
+
 }
