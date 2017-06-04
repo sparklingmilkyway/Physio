85ef65cf5e9be108c295e37cd6a1c4773c455fa3
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-06-04 19:09:45 +0200

    fe: updated home.routing.ts with AuthGuard

--- frontend/src/app/_routing/home.routing.ts
+++ frontend/src/app/_routing/home.routing.ts
@@ -1,12 +1,9 @@
-
-import {PatientComponent} from "../_components/_patient_components/patient/patient.component";
-import {PatientAddComponent} from "../_components/_patient_components/patient-add/patient-add.component";
-import {AddComponent} from "../_components/add/add.component";
-import {ExerciseAddComponent} from "../_components/_exercise_components/exercise-add/exercise-add.component";
-import {ProgrammAddComponent} from "../_components/_programm_components/programm-add/programm-add.component";
+import {AuthGuard} from "../_guards/auth.guard";
 import {HomeComponent} from "../_components/home/home.component";
+
+
 export const homeRouting = [{
-  path: 'home', component: HomeComponent
+  path: 'home', component: HomeComponent , canActivate: [AuthGuard]
 }];
 
 export const homeRoutingComponent = [
