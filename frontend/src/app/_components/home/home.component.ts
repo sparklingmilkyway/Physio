81daec5e773b28e019ac7f605b589cf24dce9a52
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-06-04 19:13:11 +0200

    fe: updated home component

--- frontend/src/app/_components/home/home.component.ts
+++ frontend/src/app/_components/home/home.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {UserService} from "../../_services/user.service";
+import {Therapeut} from "../../_classes/Therapeut";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  moduleId: module.id,
+
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  currentUser: Therapeut;
+  users: Therapeut[] = [];
+
+  constructor(private userService: UserService, private router : Router) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
-
-
+    this.loadAllUsers();
   }
+
+  deleteUser(id: number) {
+    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+  }
+
+  private loadAllUsers() {
+    this.userService.getAll().subscribe(users => { this.users = users; });
+  }
+
 
   login() {
     this.router.navigateByUrl("/login");
   }
 
+
 }
+
+
