2963024d51bd61eee7d92bd83d0803f5d5c2f9bc
sparklingmilkyway <vanessa.wuethrich@hotmail.ch>
2017-06-04 12:08:37 +0200

    added new providers and comps to app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,21 +7,27 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { PopupModule } from 'ng2-opd-popup';
-import {PatientComponent} from "./_components/patient_components/patient/patient.component";
-import {ExerciseComponent} from "./_components/exercise_components/exercise/exercise.component";
+import {PatientComponent} from "./_components/_patient_components/patient/patient.component";
+import {ExerciseComponent} from "./_components/_exercise_components/exercise/exercise.component";
 import {SearchComponent} from "./_components/search/search.component";
 import {HomeComponent} from "./_components/home/home.component";
 import {AddComponent} from "./_components/add/add.component";
 import {AppComponent} from "./app.component";
 import {PraxisService} from "./_services/praxis.service";
 import {TabComponent, TabsComponent} from "./_navigation/tabs/tabs.component";
-import { LoginComponent } from './_components/login/login.component';
-import { PatientAddComponent } from './_components/patient_components/patient-add/patient-add.component';
-import { ExerciseAddComponent } from './_components/exercise_components/exercise-add/exercise-add.component';
-import { ProgrammAddComponent } from './_components/programm_components/programm-add/programm-add.component';
-import { ExerciseSearchComponent } from './_components/exercise_components/exercise-search/exercise-search.component';
-import { PatientSearchComponent } from './_components/patient_components/patient-search/patient-search.component';
-import { ProgrammSearchComponent } from './_components/programm_components/programm-search/programm-search.component';
+import { LoginComponent } from './_components/_login_components/login/login.component';
+import { PatientAddComponent } from './_components/_patient_components/patient-add/patient-add.component';
+import { ExerciseAddComponent } from './_components/_exercise_components/exercise-add/exercise-add.component';
+import { ProgrammAddComponent } from './_components/_programm_components/programm-add/programm-add.component';
+import { ExerciseSearchComponent } from './_components/_exercise_components/exercise-search/exercise-search.component';
+import { PatientSearchComponent } from './_components/_patient_components/patient-search/patient-search.component';
+import { ProgrammSearchComponent } from './_components/_programm_components/programm-search/programm-search.component';
+import { RegisterComponent } from './_components/_login_components/register/register.component';
+import {AuthGuard} from "./_guards/auth.guard";
+import {AlertService} from "./_services/alert.service";
+import {AuthenticationService} from "./_services/authentication.service";
+import {UserService} from "./_services/user.service";
+import {AlertComponent} from "./_directives/alert.component";
 
 
 @NgModule({
@@ -41,7 +47,10 @@ import { ProgrammSearchComponent } from './_components/programm_components/progr
     ProgrammAddComponent,
     ExerciseSearchComponent,
     PatientSearchComponent,
-    ProgrammSearchComponent
+    ProgrammSearchComponent,
+    RegisterComponent,
+    AlertComponent
+
     //
   ],
   imports: [
@@ -52,7 +61,13 @@ import { ProgrammSearchComponent } from './_components/programm_components/progr
     appRouting,
     PopupModule.forRoot()
   ],
-  providers: [PraxisService],
+  providers: [
+    PraxisService,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
